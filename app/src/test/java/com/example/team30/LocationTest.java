dc472c923b228c799f93bd5b2b4a59ab547e7cbd
Pratham Rathi <pratham.02rathi@gmail.com>
2023-02-13 10:44:48 -0800

    refactoring tests

--- app/src/test/java/com/example/team30/LocationTest.java
+++ app/src/test/java/com/example/team30/LocationTest.java
@@ -5,22 +5,22 @@ import org.junit.Test;
 public class LocationTest {
     @Test
     public void testGetName() {
-        Location location = new Location("Golden Gate Bridge", "Bridge",
+        Location location = new Location("Parent", "Golden Gate Bridge",
                 new Coordinates((float) 37.7749, (float) -122.4194));
         assertEquals("Golden Gate Bridge", location.getName());
     }
 
     @Test
     public void testGetType() {
-        Location location = new Location("Golden Gate Bridge", "Bridge",
+        Location location = new Location("Friend", "Golden Gate Bridge",
                 new Coordinates((float) 37.7749, (float) -122.4194));
-        assertEquals("Bridge", location.getType());
+        assertEquals("Friend", location.getType());
     }
 
     @Test
     public void testGetCoordinates() {
         Coordinates coordinates = new Coordinates((float) 37.7749, (float) -122.4194);
-        Location location = new Location("Golden Gate Bridge", "Bridge",
+        Location location = new Location("Parent", "Mom's",
                 coordinates);
         assertEquals(coordinates, location.getCoordinates());
     }
@@ -43,7 +43,7 @@ public class LocationTest {
     public void testSetCoordinates() {
         Location location = new Location("Test location", "Type", new Coordinates(0, 0));
         location.setCoordinates(new Coordinates(1, 2));
-        assertEquals(1, location.getLongitude(), 0);
-        assertEquals(2, location.getLatitude(), 0);
+        assertEquals(1, location.getLatitude(), 0);
+        assertEquals(2, location.getLongitude(), 0);
     }
 }
