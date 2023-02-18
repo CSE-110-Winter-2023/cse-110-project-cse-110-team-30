0d79868f1bb43b2f4e52b22dadd3617005d6a055
yi yao <yiy054@ucsd.edu>
2023-02-18 15:44:48 -0800

    MergeConflict Test Change #11

--- app/src/androidTest/java/com/example/team30/BDDTest.java
+++ app/src/androidTest/java/com/example/team30/BDDTest.java
@@ -59,10 +59,10 @@ public class BDDTest {
                                         0),
                                 0),
                         isDisplayed()));
-        appCompatEditText.perform(replaceText("testParent"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText("testFriend"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.longitude),
+                allOf(withId(R.id.latitude),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
@@ -72,14 +72,14 @@ public class BDDTest {
         appCompatEditText2.perform(replaceText("2000"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.latitude),
+                allOf(withId(R.id.longitude),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
                                         0),
                                 2),
                         isDisplayed()));
-        appCompatEditText3.perform(replaceText("4000"), closeSoftKeyboard());
+        appCompatEditText3.perform(replaceText("-4000"), closeSoftKeyboard());
 
         ViewInteraction appCompatSpinner = onView(
                 allOf(withId(R.id.spinner_category),
@@ -108,16 +108,29 @@ public class BDDTest {
                         isDisplayed()));
         materialButton.perform(click());
 
+        ViewInteraction imageView = onView(
+                allOf(withId(R.id.Friendhome),
+                        withParent(allOf(withId(R.id.compass),
+                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
+                        isDisplayed()));
+        imageView.check(matches(isDisplayed()));
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.friendLabel), withText("testFriend"),
+                        withParent(allOf(withId(R.id.compass),
+                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
+                        isDisplayed()));
+        textView.check(matches(withText("testFriend")));
 
         ViewInteraction imageView2 = onView(
-                allOf(withId(R.id.compassFace),
+                allOf(withId(R.id.triangle),
                         withParent(allOf(withId(R.id.compass),
                                 withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
                         isDisplayed()));
         imageView2.check(matches(isDisplayed()));
 
         ViewInteraction imageView3 = onView(
-                allOf(withId(R.id.triangle),
+                allOf(withId(R.id.compassFace),
                         withParent(allOf(withId(R.id.compass),
                                 withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
                         isDisplayed()));
@@ -129,13 +142,190 @@ public class BDDTest {
                         isDisplayed()));
         button.check(matches(isDisplayed()));
 
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.addLoc), withText("Add Location"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.name),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatEditText4.perform(replaceText("testParent"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.latitude),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText5.perform(replaceText("0"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.longitude),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText6.perform(replaceText("0"), closeSoftKeyboard());
+
+        ViewInteraction appCompatSpinner2 = onView(
+                allOf(withId(R.id.spinner_category),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatSpinner2.perform(click());
+
+        DataInteraction materialTextView2 = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(0);
+        materialTextView2.perform(click());
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.submit), withText("Submit"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
         ViewInteraction imageView4 = onView(
                 allOf(withId(R.id.house),
                         withParent(allOf(withId(R.id.compass),
                                 withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
                         isDisplayed()));
-        imageView4.check(doesNotExist());
+        imageView4.check(matches(isDisplayed()));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.parentLabel), withText("testParent"),
+                        withParent(allOf(withId(R.id.compass),
+                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
+                        isDisplayed()));
+        textView2.check(matches(withText("testParent")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.friendLabel), withText("testFriend"),
+                        withParent(allOf(withId(R.id.compass),
+                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
+                        isDisplayed()));
+        textView3.check(matches(withText("testFriend")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.friendLabel), withText("testFriend"),
+                        withParent(allOf(withId(R.id.compass),
+                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
+                        isDisplayed()));
+        textView4.check(matches(withText("testFriend")));
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.addLoc), withText("Add Location"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.name),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatEditText7.perform(replaceText("MyHome"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText8 = onView(
+                allOf(withId(R.id.latitude),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText8.perform(replaceText("-64379"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText9 = onView(
+                allOf(withId(R.id.longitude),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText9.perform(replaceText("271893"), closeSoftKeyboard());
+
+        ViewInteraction appCompatSpinner3 = onView(
+                allOf(withId(R.id.spinner_category),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatSpinner3.perform(click());
+
+        DataInteraction materialTextView3 = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(2);
+        materialTextView3.perform(click());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.submit), withText("Submit"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton5.perform(click());
+
+        ViewInteraction imageView5 = onView(
+                allOf(withId(R.id.Myhome),
+                        withParent(allOf(withId(R.id.compass),
+                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
+                        isDisplayed()));
+        imageView5.check(matches(isDisplayed()));
+
+        ViewInteraction textView5 = onView(
+                allOf(withId(R.id.myLabel), withText("MyHome"),
+                        withParent(allOf(withId(R.id.compass),
+                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
+                        isDisplayed()));
+        textView5.check(matches(withText("MyHome")));
+
+        ViewInteraction textView6 = onView(
+                allOf(withId(R.id.myLabel), withText("MyHome"),
+                        withParent(allOf(withId(R.id.compass),
+                                withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class)))),
+                        isDisplayed()));
+        textView6.check(matches(withText("MyHome")));
     }
+
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
