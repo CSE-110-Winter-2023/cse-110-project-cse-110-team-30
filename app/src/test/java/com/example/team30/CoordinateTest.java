2b4aa802aab47a37c465437762704f7be5926915
yi yao <yiy054@ucsd.edu>
2023-02-18 14:34:36 -0800

    CompassTest & CoordinateTes #11

--- app/src/test/java/com/example/team30/CoordinateTest.java
+++ app/src/test/java/com/example/team30/CoordinateTest.java
@@ -6,14 +6,26 @@ import static org.junit.Assert.*;
     public class CoordinateTest {
         @Test
         public void testGetLatitude() {
-            Coordinates coordinates = new Coordinates((float) 37.7749, (float) -122.4194);
-            assertEquals(37.7749, coordinates.getLatitude(), 0.001);
+            Coordinates coordinates1 = new Coordinates((float) 37.7749, (float) -122.4194);
+            Coordinates coordinates2 = new Coordinates((float) -37.7749, (float) -122.4194);
+            Coordinates coordinates3 = new Coordinates((float) 0, (float) 0);
+            Coordinates coordinates4 = new Coordinates((float) -37.7749, (float) 122.4194);
+            assertEquals(37.7749, coordinates1.getLatitude(), 0.001);
+            assertEquals(-37.7749, coordinates2.getLatitude(), 0.001);
+            assertEquals(0, coordinates3.getLatitude(), 0.001);
+            assertEquals(-37.7749, coordinates4.getLatitude(), 0.001);
         }
 
         @Test
         public void testGetLongitude() {
-            Coordinates coordinates = new Coordinates((float) 37.7749, (float) -122.4194);
-            assertEquals(-122.4194, coordinates.getLongitude(), 0.001);
+            Coordinates coordinates1 = new Coordinates((float) 37.7749, (float) -122.4194);
+            Coordinates coordinates2 = new Coordinates((float) -37.7749, (float) -122.4194);
+            Coordinates coordinates3 = new Coordinates((float) 0, (float) 0);
+            Coordinates coordinates4 = new Coordinates((float) -37.7749, (float) 122.4194);
+            assertEquals(-122.4194, coordinates1.getLongitude(), 0.001);
+            assertEquals(-122.4194, coordinates2.getLongitude(), 0.001);
+            assertEquals(0, coordinates3.getLongitude(), 0.001);
+            assertEquals(122.4194, coordinates4.getLongitude(), 0.001);
         }
 
         @Test
@@ -21,6 +33,10 @@ import static org.junit.Assert.*;
             Coordinates coordinates = new Coordinates((float) 37.7749, (float) -122.4194);
             coordinates.setLatitude((float) 47.6062);
             assertEquals(47.6062, coordinates.getLatitude(), 0.001);
+            coordinates.setLatitude((float) 0);
+            assertEquals(0, coordinates.getLatitude(), 0.001);
+            coordinates.setLatitude((float) -47.6062);
+            assertEquals(-47.6062, coordinates.getLatitude(), 0.001);
         }
 
         @Test
@@ -28,6 +44,10 @@ import static org.junit.Assert.*;
             Coordinates coordinates = new Coordinates((float) 37.7749, (float) -122.4194);
             coordinates.setLongitude((float) -122.3321);
             assertEquals(-122.3321, coordinates.getLongitude(), 0.001);
+            coordinates.setLongitude((float) 0);
+            assertEquals(0, coordinates.getLongitude(), 0.001);
+            coordinates.setLongitude((float) 122.3321);
+            assertEquals(122.3321, coordinates.getLongitude(), 0.001);
         }
     }
 
