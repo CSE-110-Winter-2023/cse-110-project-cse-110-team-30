ff16ecdef038f7327a7233fca0ed1ebf1f6836ee
Pratham Rathi <pratham.02rathi@gmail.com>
2023-03-05 14:58:43 -0800

    fixed location service

--- app/src/main/java/com/example/team30/LocationService.java
+++ app/src/main/java/com/example/team30/LocationService.java
@@ -1,60 +1,111 @@
 package com.example.team30;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 
+import androidx.activity.result.contract.ActivityResultContracts.RequestMultiplePermissions;
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
+import androidx.annotation.RequiresPermission;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.Pair;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-public class LocationService implements LocationListener {
-    private static LocationService instance;
-    private Activity activity;
+import java.util.Arrays;
 
-    private MutableLiveData<Coordinates> locationValue;
+public class LocationService implements LocationListener {
+
+    final String[] REQUIRED_PERMISSIONS = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+
+    // This needs to be more specific than just Activity for location permissions requesting.
+    private final AppCompatActivity activity;
+
+    private static LocationService instance;
+
+    private MutableLiveData<Pair<Double, Double>> locationValue;
+
     private final LocationManager locationManager;
 
-    public static LocationService singleton(Activity activity){
-        if(instance == null){
+    public static LocationService singleton(AppCompatActivity activity) {
+        if (instance == null) {
             instance = new LocationService(activity);
         }
         return instance;
     }
 
-    protected LocationService(Activity activity){
+    /**
+     * Constructor for LocationService
+     *
+     * @param activity Context needed to initiate LocationManager
+     */
+    protected LocationService(AppCompatActivity activity) {
         this.locationValue = new MutableLiveData<>();
         this.activity = activity;
         this.locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
-        this.registerLocationListener();
+        // Register sensor listeners
+        withLocationPermissions(this::registerLocationListener);
     }
 
-    private void registerLocationListener(){
-        if(ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            throw new IllegalStateException("App needs permission to get latest location");
-        }
+    /**  This will only be called when we for sure have permissions. */
+    @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
+    private void registerLocationListener() {
+        this.locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0,
+                LocationService.this);
+    }
 
-        this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    /** Utility method for doing something with location permissions if we have them, and
+     *  after asking for them if we don't already.
+     * @param action the thing to do that needs permissions.
+     */
+    private void withLocationPermissions(Runnable action) {
+        if (Arrays.stream(REQUIRED_PERMISSIONS).allMatch(perm -> activity.checkSelfPermission(perm) == PackageManager.PERMISSION_GRANTED)) {
+            // We already have at least one of the location permissions, go ahead!
+            action.run();
+        } else {
+            // We need to ask for permission first.
+            // This is the call that requires AppCompatActivity and not just Activity!
+            var launcher = activity.registerForActivityResult(new RequestMultiplePermissions(), grants -> {
+                // At least one of the values in the Map<String, Boolean> grants needs to be true.
+                if (grants.values().stream().noneMatch(isGranted -> isGranted)) {
+                    // If you've landed here by denying it, you should grant it manually in settings or wipe data.
+                    throw new IllegalStateException("App needs you to grant at least one location permission!");
+                }
+                // We have permission now, carry on!
+                action.run();
+            });
+            launcher.launch(REQUIRED_PERMISSIONS);
+        }
     }
 
     @Override
-    public void onLocationChanged(@NonNull Location location){
-        Coordinates current = new Coordinates((float)location.getLatitude(), (float)location.getLongitude());
-        this.locationValue.postValue(current);
+    public void onLocationChanged(@NonNull Location location) {
+        this.locationValue.postValue(new Pair<>(location.getLatitude(), location.getLongitude()));
     }
 
-    private void unregisterLocationListener(){ locationManager.removeUpdates(this);}
+    private void unregisterLocationListener() {
+        locationManager.removeUpdates(this);
+    }
 
-    public LiveData<Coordinates> getLocation(){ return this.locationValue; }
+    public LiveData<Pair<Double, Double>> getLocation() {
+        return this.locationValue;
+    }
 
-    public void setMockOrientationSource(MutableLiveData<Coordinates> mockDataSource){
+    public void setMockOrientationData(MutableLiveData<Pair<Double, Double>> mockData) {
         unregisterLocationListener();
-        this.locationValue = mockDataSource;
+        this.locationValue = mockData;
     }
 }
