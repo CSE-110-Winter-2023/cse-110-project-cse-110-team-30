be38f70bf7233e36c2c62d11a80dc2b54157bb04
Cthloveross <chentianhao020912@163.com>
2023-03-08 00:19:05 -0800

    #55
    compass functions

--- app/src/main/java/com/example/team30/Compass.java
+++ app/src/main/java/com/example/team30/Compass.java
@@ -1,20 +1,69 @@
 package com.example.team30;
 
+import android.util.Pair;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.lang.Math;
 
 public class Compass {
-    private Map<String, Float> locations;
+    private float myLong;
+
+    private float myLat;
+
+    private Map<String, String> IDlist;
+
+    private Map<String, Pair<Float, Integer>> position;
 
     private static Compass instance;
 
+    public static  Compass singleton(){
+        if(instance == null){
+            instance = new Compass();
+        }
+        return instance;
+    }
+
     public Compass(){
-        locations = new HashMap<>();
+        IDlist = new HashMap<>();
+        position = new HashMap<>();
+    }
+
+    public float getmyLong(){
+        return myLong;
+    }
+
+    public float getMyLat(){
+        return myLat;
+    }
+
+    public void setMyLong(float newLong){
+        this.myLong = newLong;
+    }
+
+    public void  setMyLat(float newLat){
+        this.myLat = newLat;
     }
 
     public void calculateAngles(){
 
     }
 
+    public void addPosition(String objectId, Pair<Float,Integer> newPair){
+        position.put(objectId, newPair);
+    }
+
+
+    public Pair<Float, Integer> getPosition(String objectId){
+        return position.get(objectId);
+    }
+
+    public void setPosition(String objectId, Pair<Float, Integer> newPair){
+        position.put(objectId, newPair);
+    }
+
+
+    public void addIDObject(String UID, String objectId){
+
+    }
 }
