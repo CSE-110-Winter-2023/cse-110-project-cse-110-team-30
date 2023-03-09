74682b61f22ef1810398a76a89c36b3acde1e75f
yi yao <yiy054@ucsd.edu>
2023-03-08 23:06:52 -0800

    BDDTest of iteration1 #45

---
+++ app/src/androidTest/java/com/example/team30/Iteration1BDDTest.java
@@ -0,0 +1,216 @@
+package com.example.team30;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.GrantPermissionRule;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class Iteration1BDDTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Rule
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.ACCESS_FINE_LOCATION",
+                    "android.permission.ACCESS_COARSE_LOCATION");
+
+    @Test
+    public void iteration1BDDTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.UsernameEntry),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("testName"), closeSoftKeyboard());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.UID_generator), withText("Generate UID"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.PromptToShare), withText("Share it with your friends and family!"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        textView.check(matches(withText("Share it with your friends and family!")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.UIDGenTitle), withText("Here is your UID"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView2.check(matches(withText("Here is your UID")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.NewUIDDisplay), withText("fec41689-b5c5-40f2-afa7-fcbf127d2b27"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        textView3.check(matches(isDisplayed()));
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.GoToCompass), withText("GO TO COMPASS"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.GoToCompass), withText("GO TO COMPASS"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        button2.check(matches(isDisplayed()));
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.GoToCompass), withText("Go to Compass"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.zoom_in), withText("+"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+                        isDisplayed()));
+        button3.check(matches(isDisplayed()));
+
+        ViewInteraction button4 = onView(
+                allOf(withId(R.id.zoom_out), withText("-"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+                        isDisplayed()));
+        button4.check(matches(isDisplayed()));
+
+        ViewInteraction button5 = onView(
+                allOf(withId(R.id.addLoc), withText("ADD FRIEND"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        button5.check(matches(isDisplayed()));
+
+        ViewInteraction imageView = onView(
+                allOf(withId(R.id.triangle),
+                        withParent(allOf(withId(R.id.compass),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class)))),
+                        isDisplayed()));
+        imageView.check(matches(isDisplayed()));
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.addLoc), withText("Add Friend"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.YourUIDDisplay), withText("fec41689-b5c5-40f2-afa7-fcbf127d2b27"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        textView4.check(matches(isDisplayed()));
+
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.FriendsUIDEntry), withText("Friend's UID"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        editText.check(matches(withText("Friend's UID")));
+
+        ViewInteraction button6 = onView(
+                allOf(withId(R.id.submitBtn), withText("SUBMIT"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        button6.check(matches(isDisplayed()));
+
+        ViewInteraction textView5 = onView(
+                allOf(withId(R.id.AddFriendTitle), withText("Add friend"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView5.check(matches(withText("Add friend")));
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.FriendsUIDEntry),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                3),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("hantian1"), closeSoftKeyboard());
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.submitBtn), withText("Submit"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction imageView2 = onView(
+                allOf(withId(R.id.triangle),
+                        withParent(allOf(withId(R.id.compass),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class)))),
+                        isDisplayed()));
+        imageView2.check(matches(isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
