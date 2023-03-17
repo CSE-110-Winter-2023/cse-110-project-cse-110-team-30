d6dd87298bb37c330f5f51de1724fabc3dfd8b77
Pratham Rathi <35015289+PrathamRathi@users.noreply.github.com>
2023-03-16 21:50:50 -0700

    finished tests

--- app/src/test/java/com/example/team30/CompassTest.java
+++ app/src/test/java/com/example/team30/CompassTest.java
@@ -18,4 +18,22 @@ public class CompassTest {
         assertEquals(11.5, compass.getMyLat(), .001);
         assertEquals(31.6, compass.getMyLong(), .001);
     }
+
+    @Test
+    public void testZoom0Radius(){
+        assertEquals(20, compass.zoom0radius(.5), .01);
+        assertEquals(82.376, compass.zoom0radius(39.11), .01);
+    }
+
+    @Test
+    public void testZoom1Radius(){
+        assertEquals(26.65, compass.zoom1radius(.5), .01);
+        assertEquals(109.77, compass.zoom1radius(39.11), .01);
+    }
+
+    @Test
+    public void testZoom2Radius(){
+        assertEquals(40, compass.zoom2radius(.5), .01);
+        assertEquals(160, compass.zoom2radius(39.11), .01);
+    }
 }
